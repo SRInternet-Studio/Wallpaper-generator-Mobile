9ce11772fe3a21013bf31bddfb1a5cc780bf66ec
faithleysath <faithleysath@gmail.com>
2025-08-08 23:19:23 +0800

    Revert "修了下构建bug"
    This reverts commit fda7efeea662744844d6a96011f0e493530d9e06.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { resolve } from "path";
-import { fileURLToPath } from "url";
 
+// @ts-expect-error process is a nodejs global
 const host = process.env.TAURI_DEV_HOST;
 
 // https://vitejs.dev/config/
@@ -33,8 +32,8 @@ export default defineConfig(async () => ({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(fileURLToPath(new URL('.', import.meta.url)), "index.html"),
-        splashscreen: resolve(fileURLToPath(new URL('.', import.meta.url)), "splashscreen.html"),
+        main: "index.html",
+        splashscreen: "splashscreen.html",
       },
     },
   },
